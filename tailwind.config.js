36d9effc5d8267d26d76c89259c992240e731631
PRONOB BISWAS <biswaspronob@outlook.com>
2024-01-02 02:48:08 +0600

    may first node project with github and this the fist deploymet in life

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        }
+      },
+    },
+    plugins: [],
+  }
+  
